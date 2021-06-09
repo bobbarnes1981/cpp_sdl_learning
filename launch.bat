4d8eb71a920729d3eab4e6a275c6f3f686c94878
Robert Barnes <Robert.Barnes@btl.com>
2021-06-09 15:46:50 +0100

    Initial commit

---
+++ launch.bat
@@ -0,0 +1,4 @@
+echo off
+set PATH=C:\Program Files (x86)\mingw-w64\i686-7.1.0-win32-dwarf-rt_v5-rev2\mingw32\bin;D:\Learning\SDL2-devel-2.0.14-mingw\SDL2-2.0.14\i686-w64-mingw32\bin;%PATH%
+REM cd "C:\"
+"C:\WINDOWS\system32\cmd.exe"
