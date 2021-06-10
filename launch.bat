46662c9059ce9333e15f5c720f7132cda2061210
Robert Barnes <Robert.Barnes@btl.com>
2021-06-10 09:46:23 +0100

    Use program files for mingw

--- launch.bat
+++ launch.bat
@@ -1,4 +1,5 @@
 echo off
 SET PWD=%CD%
-set PATH=%PWD%\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin;%PWD%\SDL2-2.0.14\i686-w64-mingw32\bin;%PWD%\SDL2_image-2.0.5\i686-w64-mingw32\bin;%PATH%
+SET PROGS=c:\Program Files (x86)
+set PATH=%PROGS%\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin;%PWD%\SDL2-2.0.14\i686-w64-mingw32\bin;%PWD%\SDL2_image-2.0.5\i686-w64-mingw32\bin;%PATH%
 "C:\WINDOWS\system32\cmd.exe"
