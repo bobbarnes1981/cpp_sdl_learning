e98bab7092148221da023c4cc39f96c47e406bc5
Robert Barnes <Robert.Barnes@btl.com>
2021-06-11 13:05:32 +0100

    Add sdl ttf library to launch

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 echo off
 SET PWD=%CD%
 SET PROGS=c:\Program Files (x86)
-set PATH=%PROGS%\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin;%PWD%\SDL2-2.0.14\i686-w64-mingw32\bin;%PWD%\SDL2_image-2.0.5\i686-w64-mingw32\bin;%PATH%
+set PATH=%PROGS%\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin;%PWD%\SDL2-2.0.14\i686-w64-mingw32\bin;%PWD%\SDL2_image-2.0.5\i686-w64-mingw32\bin;%PWD%\SDL2_ttf-2.0.15\i686-w64-mingw32\bin;%PATH%
 "C:\WINDOWS\system32\cmd.exe"
