896c8b6c260606c236bace18d839c28ae79e5693
Robert Barnes <Robert.Barnes@btl.com>
2021-06-14 14:42:49 +0100

    Add icons files

---
+++ Computer/icons.cpp
@@ -0,0 +1,11 @@
+#include "icons.h"
+
+Icons::Icons()
+{
+    
+};
+
+Icons::~Icons()
+{
+    
+};
---
+++ Computer/icons.h
@@ -0,0 +1,11 @@
+#ifndef __h_icons
+#define __h_icons
+
+class Icons
+{
+    public:
+        Icons();
+        ~Icons();
+};
+
+#endif
